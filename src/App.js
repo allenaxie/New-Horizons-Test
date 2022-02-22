e4c43d9031b3a428b8c63fc3be81b88e9fba3c73
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-21 18:54:22 -0800

    Create modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+import { useState } from "react";
+import {Form, Input, Button, Modal} from 'antd';
 
 
 function App() {
+
+  // Modal
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  function showModal () {
+    setIsModalVisible(true)
+  }
+  function closeModal() {
+    setIsModalVisible(false)
+  }
+
+  // Form
+  
+
   return (
     <div className="App">
       <header className="App-header">
-       
+       <Button type="primary" onClick={showModal}>
+         Click Me!
+       </Button>
+       <Modal title="Submit Expense" visible={isModalVisible} onOk={closeModal} onCancel={closeModal}>
+        <Form>
+          hey
+        </Form>
+       </Modal>
       </header>
     </div>
   );
